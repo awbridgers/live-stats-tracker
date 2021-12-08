74ae0b66afba51ba0765c98a3f367a5af31764ed
Adam Bridgers <awbridgers@users.noreply.github.com>
2021-12-08 12:07:45 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import {getAllJSDocTagsOfKind} from 'typescript';
 import './App.css';
 
 function App() {
+  const handleSubmit = () =>{
+    console.log('test')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea className="textBox"></textarea>
+      <button className = 'submit' type = 'button' onClick = {handleSubmit}>Submit</button>
     </div>
   );
 }
